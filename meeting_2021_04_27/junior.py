614db0bdac0df94bf795e45b46dbed7ca1b7e60a
AnnaNordstrm <82153039+AnnaNordstrm@users.noreply.github.com>
2021-04-27 17:23:06 +0200

    Add files via upload

---
+++ meeting_2021_04_27/junior.py
@@ -0,0 +1,28 @@
+
+from flask import Flask, request, render_template, jsonify
+from middleware import join_args, food_level, delta_time, current_time
+#from deltatime import delta_time
+
+app = Flask(__name__)
+
+
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/join', methods=['GET','POST'])
+def join():
+    #text1 = request.form['text1']
+    #text2 = request.form['text2']
+
+    hunger = food_level(True, delta_time(2104261400), 40)
+    result = {
+        "result": hunger
+ 
+    }
+    print(hunger)
+    return jsonify(result=result)
+
+if __name__ == '__main__':
+    app.run(debug=True)
