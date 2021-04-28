3cdd8de709f971632bae934a28a1747229fbda18
AnnaNordstrm <82153039+AnnaNordstrm@users.noreply.github.com>
2021-04-28 14:05:20 +0200

    Update joke_dictionary.py

--- middleware_anna/joke_dictionary.py
+++ middleware_anna/joke_dictionary.py
@@ -1,24 +1,45 @@
 from random import randint
 
-buildUpsD = {}
+
+#_____________________________________________
+buildUpsD = {}          # These dictionaries should probably be in the main program in our finished product
 punchLinesD = {}
 karmaPointsD = {}
+#_____________________________________________
+
+
 
 def jokeMaker (b,p):         #buildup, punchline, karma points
+    """
+    Purpose: Places each element of the joke in its datastorage place.
+    Paramters: 
+    b - the first, obligatory part of the userprovided joke input.
+    p - the latter, optional part of the joke.
+    Returns: None.
+    """
     buildUpsD ["Buildup" + (str(len(buildUpsD)+1))] = b 
     punchLinesD ["Punchline" + (str(len(punchLinesD)+1))] = p 
     karmaPointsD ["Karma points" + (str(len(karmaPointsD)+1))] = 10 
-                                            # Standardpoäng vid insättnig är ust nu 10 
+                                            # Standardpoäng vid insättnig är just nu 10 
 
 
 def jokeProvider ():
+    """
+    Purpose: Picks and gives out a random joke. 
+    TODO: Should maybe be modified to pick more highly voted jokes more often
+    Paramters: None
+    Returns: A tuple consisting of two strings. 
+    """
     randNr = randint(1, len(buildUpsD))
     buildUp = buildUpsD["Buildup" + str(randNr)]
     punchLine = punchLinesD["Punchline" + str(randNr)]
     karma = karmaPointsD["Karma points" + str(randNr)]
 
-    print(buildUp)
-    print(punchLine)
+    return buildUp
+    return punchLine
+
+    
+ #________________TESTCODE____________________________#
 
 jokeMaker("Orange you glad", "to see me")
 
