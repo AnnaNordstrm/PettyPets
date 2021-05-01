411797fac26511529f5654716087f6618c5fc32e
AnnaNordstrm <82153039+AnnaNordstrm@users.noreply.github.com>
2021-05-02 01:17:43 +0200

    Update frankensteinJunior.py

--- middleware_anna/frankensteinJunior.py
+++ middleware_anna/frankensteinJunior.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('home_anna.html')
+    return render_template('home.html')
 
 @app.route('/join', methods=['GET','POST'])
 def join():
