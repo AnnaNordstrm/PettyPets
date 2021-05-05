fe78b8d48e1d8a09cfc1fa59e6483214f073e2b2
Isabelle Teglund <63157218+Lundisa00@users.noreply.github.com>
2021-05-05 14:46:25 +0200

    Update junior_isabelle.py

--- junior_isabelle/junior_isabelle.py
+++ junior_isabelle/junior_isabelle.py
@@ -2,9 +2,20 @@ from flask import Flask, request, render_template, jsonify
 
 app = Flask(__name__)
 
+# app.route "/" används för att logga in
 @app.route('/')
 def home():
     return render_template('home_isabelle.html')
+
+# Skapa app.route "/sign_up" för att skapa konto
+
+# Skapa app.route "/home" för startsidan
+
+# Skapa app.route "/jokes" för skämtsidan
+
+# Skapa app.route "/about" för information om projektet
+
+# Skapa app.route "/sign_out" för utloggningssidan
 
 if __name__ == '__main__':
     app.run(debug=True)
