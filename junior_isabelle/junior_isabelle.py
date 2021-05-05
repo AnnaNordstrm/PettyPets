e62c60811a020f8606344f3de495790f1fc769e0
Isabelle Teglund <63157218+Lundisa00@users.noreply.github.com>
2021-05-05 14:43:19 +0200

    Update junior_isabelle.py

--- junior_isabelle/junior_isabelle.py
+++ junior_isabelle/junior_isabelle.py
@@ -1,20 +1,10 @@
 from flask import Flask, request, render_template, jsonify
-from middleware_main import join_args
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('home_main.html')
-
-@app.route('/join', methods=['GET','POST'])
-def join():
-    text1 = request.form['text1']
-    text2 = request.form['text2']
-    result = {
-        "result": join_args(text1,text2)
-    }
-    return jsonify(result=result)
+    return render_template('home_isabelle.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
