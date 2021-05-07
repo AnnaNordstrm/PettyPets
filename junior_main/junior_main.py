52040ef40a5e0d81d882bb921bd591f9976fc840
Isabelle Teglund <63157218+Lundisa00@users.noreply.github.com>
2021-05-07 12:10:31 +0200

    Update junior_main.py

--- junior_main/junior_main.py
+++ junior_main/junior_main.py
@@ -1,20 +1,30 @@
 from flask import Flask, request, render_template, jsonify
-from middleware_main import join_args
 
 app = Flask(__name__)
 
 @app.route('/')
-def home():
-    return render_template('home_main.html')
+def sign_in():
+    return render_template('home_isabelle.html')
 
-@app.route('/join', methods=['GET','POST'])
-def join():
-    text1 = request.form['text1']
-    text2 = request.form['text2']
-    result = {
-        "result": join_args(text1,text2)
-    }
-    return jsonify(result=result)
+@app.route('/sign_up')
+def sign_up():
+    return render_template('home_isabelle.html')
+
+@app.route('/home')
+def home():
+    return render_template('home_isabelle.html')
+
+@app.route('/jokes')
+def jokes():
+    return render_template('home_isabelle.html')
+
+@app.route('/about')
+def about():
+    return render_template('home_isabelle.html')
+
+@app.route('/sign_out')
+def sign_out():
+    return render_template('home_isabelle.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
