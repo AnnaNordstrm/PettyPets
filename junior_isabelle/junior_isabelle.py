f4dcf824206514731afa1331cbadfb9964fae269
Isabelle Teglund <63157218+Lundisa00@users.noreply.github.com>
2021-05-11 12:07:08 +0200

    Add files via upload

--- junior_isabelle/junior_isabelle.py
+++ junior_isabelle/junior_isabelle.py
@@ -1,14 +1,16 @@
+# Flask is a web application framework
+
 from flask import Flask, request, render_template, jsonify
 
 app = Flask(__name__)
 
 @app.route('/')
 def sign_in():
-    return render_template('home_isabelle.html')
+    return render_template('sign_in_isabelle.html')
 
-@app.route('/sign_up')
+@app.route('/sign-up')
 def sign_up():
-    return render_template('home_isabelle.html')
+    return render_template('sign_up_isabelle.html')
 
 @app.route('/home')
 def home():
@@ -16,15 +18,15 @@ def home():
 
 @app.route('/jokes')
 def jokes():
-    return render_template('home_isabelle.html')
+    return render_template('jokes_isabelle.html')
 
 @app.route('/about')
 def about():
-    return render_template('home_isabelle.html')
+    return render_template('about_isabelle.html')
 
-@app.route('/sign_out')
+@app.route('/sign-out')
 def sign_out():
-    return render_template('home_isabelle.html')
+    return render_template('sign_out_isabelle.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
