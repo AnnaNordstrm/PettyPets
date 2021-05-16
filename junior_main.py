d949aa187ee6f605152d76d12938e5170b1fc66f
Isabelle Teglund <63157218+Lundisa00@users.noreply.github.com>
2021-05-16 21:35:56 +0200

    Add files via upload

--- junior_main.py
+++ junior_main.py
@@ -1,12 +1,48 @@
 from flask import Flask, request, render_template, jsonify
-#from middleware_main import Pet, joke_provider
-#import time
-#from deltatime import delta_time
-#from get_time import *
-#import json
-#from random import randint
+from middleware_main import joke_provider, delta_time, current_time
+import time
+from datetime import datetime
+import json
+from random import randint
 
 app = Flask(__name__)
+
+class Pet:
+   #  Class for the pet
+   def __init__(self):
+      self.name = "Lemmy"
+      self.timeOfBirth = current_time()
+      self.foodLevel = 30
+      self.lastFed = current_time()
+      self.mood = "angry"
+      self.feed = False
+
+   """
+   Purpose: To make the object hungrier with time, and if right conditions: feeds the pet +30
+   Parameters:
+   tryFeed: Boolean, takes in whether user tries to feed the pet.
+   Returns: an updated self, mood and feed
+   """
+
+   def food_level(self, tryFeed):
+
+      deltatime = delta_time(self.lastFed)[0]
+      self.foodLevel = self.foodLevel - (0.05 * deltatime)
+
+      if self.foodLevel >= 70 and tryFeed:
+         self.feed = False
+         self.mood = 'happy'
+      elif self.foodLevel > 0 and tryFeed:
+         self.foodLevel += 30
+         self.feed = True
+         self.mood = 'happy'
+      elif self.foodLevel < 30 and self.foodLevel > 0:
+         self.mood = 'angry'
+      elif self.foodLevel <= 0:
+         self.mood = 'dead'
+
+
+pets = [Pet()]
 
 @app.route('/', methods=['GET','POST'])
 def sign_in():
@@ -33,7 +69,7 @@ def home():
     #         "Mood": pets[0].mood,
     #     }
     return render_template('home_main.html')
-    #return jsonify(esult=result))
+    # return jsonify(esult=result))
 
 @app.route('/jokes', methods=['GET','POST'])
 def jokes():
--- middleware_main.py
+++ middleware_main.py
@@ -1,51 +1,10 @@
-# CLASSES AND FUNCTIONS IN THE PROGRAM
-
-# Class for the pet
-
-class Pet:
-   def __init__(self):
-      self.name = "Lemmy"
-      self.timeOfBirth = current_time()
-      self.foodLevel = 30
-      self.lastFed = current_time()
-      self.mood = "angry"
-      self.feed = False
-
-   """
-   Purpose: To make the object hungrier with time, and if right conditions: feeds the pet +30
-   Parameters:
-   tryFeed: Boolean, takes in whether user tries to feed the pet.
-   Returns: an updated self, mood and feed
-   """
-
-   def food_level(self, tryFeed):
-
-      deltatime = delta_time(self.lastFed)[0]
-      self.foodLevel = self.foodLevel - (0.05 * deltatime)
-
-      if self.foodLevel >= 70 and tryFeed:
-         self.feed = False
-         self.mood = 'happy'
-      elif self.foodLevel > 0 and tryFeed:
-         self.foodLevel += 30
-         self.feed = True
-         self.mood = 'happy'
-      elif self.foodLevel < 30 and self.foodLevel > 0:
-         self.mood = 'angry'
-      elif self.foodLevel <= 0:
-         self.mood = 'dead'
-
-
-pets = [Pet()]
-
-# Function for jokes
-
 def joke_provider():
    """
    Purpose: Pick a joke from punchline.dat and buildup.dat.
    Returns: A list consisting of two strings, in which
    the first item is the buildup, the second is the punchline.
    """
+
    # first, pick out a random buildup:
    with open('buildup.dat') as buildups:
       buildup_data = buildups.read()
@@ -64,6 +23,48 @@ def joke_provider():
    return [buildup, punchline]
 
 # print(joke_provider())
+
+def delta_time (last_update):
+   # Takes the last feeding time, time now and returns the difference in minutes
+
+    last_feeding_time = current_time()
+    now = str(current_time())
+    last = str(last_update)
+    temp_now = []
+    temp_last = []
+
+    for i in range(5):                                  # lagrar tidsenheterna i lista
+        m = 2*i
+        temp_last.insert(i, int(last[m]+last[m+1]))
+        temp_now.insert(i, int((now[m]+now[m+1])))
+
+    time_koefficients = [525948.766, 43829.0639, 1440, 60, 1]           # allt blir i sekunder
+    deltatime = 0
+
+    for i in range(5):
+        deltatime = deltatime + (temp_now[i]-temp_last[i])*time_koefficients[i]
+
+    return deltatime, last_feeding_time
+
+from datetime import datetime
+
+def current_time():
+   # Gives current date on the form YYMMDDHHMM
+
+    time = str(datetime.now())          # Sets the variable time to current time in form of a string
+    list_remove = (' ', ':', '-', '.')   # List of characters that will be removed from time
+    time = time[2:]                     # Removes the first two numbers of the year
+    for i in list_remove:
+        time = time.replace(i,'')          # Removes items in list_remove
+
+    time = round(int(time) / 100000000)     # Turns time to an integer and removes anything lower than seconds
+
+    return time
+
+
+
+
+
 
 
 
